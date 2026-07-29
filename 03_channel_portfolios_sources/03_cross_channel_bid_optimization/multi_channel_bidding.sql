9df4dc7a96af4b344b26a78ef4ed1b44533537d3
cc9404 <cc9404@nyu.edu>
2026-07-29 17:10:00 +0800

    Add files via upload

---
+++ 03_channel_portfolios_sources/03_cross_channel_bid_optimization/multi_channel_bidding.sql
@@ -0,0 +1,21 @@
+USE mavenfuzzyfactory;
+-- multi_channel_bidding
+/* pull nonbrand conversion rates from session to order for gsearch and bsearch,
+slice the data by device type
+*/
+
+SELECT
+website_sessions.device_type AS device_type,
+website_sessions.utm_source AS utm_source,
+COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+COUNT(DISTINCT orders.order_id) AS orders,
+COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate
+FROM website_sessions
+	LEFT JOIN orders
+		ON orders.website_session_id = website_sessions.website_session_id
+WHERE website_sessions.created_at BETWEEN '2012-08-22' AND '2012-09-18'
+	AND website_sessions.utm_campaign = 'nonbrand'
+GROUP BY
+website_sessions.device_type,
+website_sessions.utm_source
+
