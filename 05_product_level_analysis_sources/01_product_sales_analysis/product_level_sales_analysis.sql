9fefb032190bbba1b1218f4ec5e61c533f98bedf
cc9404 <cc9404@nyu.edu>
2026-07-30 17:24:09 +0800

    Add files via upload

---
+++ 05_product_level_analysis_sources/01_product_sales_analysis/product_level_sales_analysis.sql
@@ -0,0 +1,18 @@
+USE mavenfuzzyfactory;
+
+/*
+-- Sales Trends
+-- product_level_sales_analysis
+pull monthly trends to date for number of sales, total revenue, and total margin
+generated
+*/
+
+SELECT DISTINCT
+	YEAR(created_at) AS yr,
+	MONTH(created_at) AS mo,
+	COUNT(DISTINCT order_id) AS number_of_sales,
+	SUM(price_usd) AS total_revenue,
+    SUM(price_usd - cogs_usd) AS total_margin
+FROM orders
+WHERE created_at < '2013-01-04'
+GROUP BY 1,2;
