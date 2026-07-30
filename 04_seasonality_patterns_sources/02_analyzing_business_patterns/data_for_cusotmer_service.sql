d7c06fbc6aa9d0b0e06d615e6688a87dc5bc7d03
cc9404 <cc9404@nyu.edu>
2026-07-30 15:16:06 +0800

    Add files via upload

---
+++ 04_seasonality_patterns_sources/02_analyzing_business_patterns/data_for_cusotmer_service.sql
@@ -0,0 +1,53 @@
+ USE mavenfuzzyfactory;
+ 
+ /*
+ -- data for cusotmer service
+ analyze the average website session volume, by hour of day and by day week
+ */
+
+-- step_1 
+SELECT *
+FROM(
+SELECT
+	DATE(created_at) AS created_date,
+    WEEKDAY(created_at) AS wkday,
+    HOUR(created_at) AS hr,
+    COUNT(DISTINCT website_session_id) AS website_sessions
+FROM website_sessions
+WHERE created_at BETWEEN '2012-09-15' AND '2012-11-15'
+GROUP BY 1,2,3
+) AS daily_houly_sessions;
+
+--
+SELECT
+	hr,
+	ROUND(AVG(website_sessions),1) AS avg_sessions,
+	ROUND(AVG(CASE WHEN wkday = 0 THEN website_sessions ELSE NULL END),1)AS mon,
+    ROUND(AVG(CASE WHEN wkday = 1 THEN website_sessions ELSE NULL END),1) AS tues,
+    ROUND(AVG(CASE WHEN wkday = 2 THEN website_sessions ELSE NULL END),1) AS wed,
+    ROUND(AVG(CASE WHEN wkday = 3 THEN website_sessions ELSE NULL END),1) AS thurs,
+    ROUND(AVG(CASE WHEN wkday = 4 THEN website_sessions ELSE NULL END),1) AS fri,
+    ROUND(AVG(CASE WHEN wkday = 5 THEN website_sessions ELSE NULL END),1) AS sat,
+    ROUND(AVG(CASE WHEN wkday = 6 THEN website_sessions ELSE NULL END),1) AS sun
+FROM(
+SELECT
+	DATE(created_at) AS created_date,
+    WEEKDAY(created_at) AS wkday,
+    HOUR(created_at) AS hr,
+    COUNT(DISTINCT website_session_id) AS website_sessions
+FROM website_sessions
+WHERE created_at BETWEEN '2012-09-15' AND '2012-11-15'
+GROUP BY 1,2,3
+) AS daily_houly_sessions
+GROUP BY 1
+ORDER BY 1;
+
+
+
+
+
+
+
+
+
+
