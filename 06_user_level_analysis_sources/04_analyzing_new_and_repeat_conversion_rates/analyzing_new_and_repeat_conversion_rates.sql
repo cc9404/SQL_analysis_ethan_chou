f66dc0bedc5ffdd486dc3fff07c7bfdec06e6a4a
cc9404 <cc9404@nyu.edu>
2026-08-03 16:40:01 +0800

    Add files via upload

---
+++ 06_user_level_analysis_sources/04_analyzing_new_and_repeat_conversion_rates/analyzing_new_and_repeat_conversion_rates.sql
@@ -0,0 +1,18 @@
+USE mavenfuzzyfactory;
+/*
+analyzing_new_and_repeat_conversion_rates
+*/
+
+SELECT
+	is_repeat_session,
+    COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
+    COUNT(DISTINCT orders.order_id) / COUNT(DISTINCT website_sessions.website_session_id) AS conv_rate,
+	SUM(price_usd)  / COUNT(DISTINCT website_sessions.website_session_id) AS rev_per_session
+
+FROM website_sessions
+	LEFT JOIN orders
+		ON website_sessions.website_session_id = orders.website_session_id
+WHERE website_sessions.created_at < '2014-11-08'
+	AND website_sessions.created_at >= '2014-01-01'
+GROUP BY 1;
+
